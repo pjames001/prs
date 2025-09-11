19dd6affc0b28dab4856fa5342a3a1c69323dbc4
philip <pjames@smartstaffingsupport.com>
2025-09-11 09:54:24 -0700

    added meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,10 @@
 'use client'
 import { Metadata } from 'next'
 import { ReactLenis } from "lenis/react";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   subsets: ["latin"],
